134fe3ca8cde55bb9ef6807e3632a3c5ae0d7979
Kalaiarasan <kalaiarasankaruppaiya@gmail.com>
2024-10-09 09:35:36 +0530

    deleted extra files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import ToDoList from './ToDoList_Project1/ToDoList';
+// import DigitalClock from './DigitalClock_Project2/DigitalClock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ToDoList/>
+      {/* <DigitalClock/> */}
     </div>
   );
 }
